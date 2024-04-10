6895192b1bf7de6e1ddcb66122044dabbeddedae
vaseksindelaru <vaseksindelaru@gmail.com>
2024-04-10 12:42:44 +0200

    tema de hoy

--- app/page.js
+++ app/page.js
@@ -2,12 +2,15 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-function Index() {
-    const [message, setMessage] = useState("Loading");
-    const router = useRouter();
 
-    useEffect(() => {
-        const fetchData = async () => {
+function Index() {
+    const router = useRouter()
+    console.log()
+    
+    const [message, setMessage] = useState("Que estudiare hoy?");
+ 
+
+        /*const fetchData = async () => {
             try {
                 const response = await fetch("http://127.0.0.1:8080/postgres.py");
                 if (!response.ok) {
@@ -17,11 +20,11 @@ function Index() {
                 let numeroTexto = "Número desconocido";
 
                 switch (data.random_number) {
-                    case 1: router.push('http://localhost:3000/page1');
-                        
+                    case 1:
+                        numeroTexto = "Props";
                         break;
                     case 2:
-                        numeroTexto = "dos";
+                        numeroTexto = "Supabase";
                         break;
                     case 3:
                         numeroTexto = "tres";
@@ -35,24 +38,62 @@ function Index() {
                     default:
                         break;
                 }
-                setMessage(numeroTexto);
+             ;
             } catch (error) {
                 console.error('Error fetching data:', error);
                 setMessage("Error al cargar los datos");
             }
-        };
-        fetchData();
-    }, []); // Asegúrate de que el enrutador esté listo antes de acceder a él
+           
+        //fetchData();
+  ;*/
+ 
 
-    const handleRefresh = () => {
-        window.location.reload();
+    const handleRefresh = async () => {
+        try {
+            const response = await fetch("http://127.0.0.1:8080/postgres.py");
+            if (!response.ok) {
+                throw new Error('Failed to fetch data');
+            }
+            const data = await response.json();
+            let destino = "/";
+
+            switch (data.random_number) {
+
+                case 1:
+                    setMessage("Props");
+                    destino = '/page1';
+                    break;
+                case 2:
+                    setMessage("Supabase");
+                    destino = '/page2';
+                    break;
+                default:
+                    break;
+                    
+            }
+             //setMessage(data.random_number);
+             setTimeout(function(){
+                router.push(destino)
+                console.log("Hola Mundo");
+            }, 3000);
+             ;
+        } catch (error) {
+
+            
+            console.error('Error fetching data:', error);
+            setMessage("Error al cargar los datos");
+        }
     };
+    console.log("Hola Mundo");
 
-    return (
+   
+return (
         <div className="text-center py-28">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleRefresh}>Refrescar página</button>
-            <div>El número es:</div>
-            <div className="text-xl font-semibold">{message}</div>
+           
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleRefresh}>Elegir</button>
+            <div>El tema es:</div>
+            <div className="text-xl font-semibold">
+                {message}</div>
         </div>
     );
 }
--- app/page1/page.js
+++ app/page1/page.js
@@ -1,11 +1,23 @@
 
+import Index from "../page"
 
-const page1 = () => {
+const Page1 = () => {
+  
   return (
     <div>
-      <h1>page1</h1>
+      
+         <form class="max-w-2xl bg-white rounded-lg border p-3 mx-auto mt-20">
+          <p>Page1</p>
+    <div class="px-3 mb-2 mt-2">
+        <textarea placeholder="comment" class="w-full bg-gray-100 rounded border border-gray-400 leading-normal resize-none h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"></textarea>
+    </div>
+    <div class="flex justify-end px-4">
+        <input type="submit" class="px-2.5 py-1.5 rounded-md text-white text-sm bg-indigo-500" value="Comment"/>
+    </div>
+</form>
+
     </div>
   )
 }
 
-export default page1
+export default Page1
