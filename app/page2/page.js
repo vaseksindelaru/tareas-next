430dfd1da6883fd182a5e4b944d756d139853139
vaseksindelaru <vaseksindelaru@gmail.com>
2024-04-30 06:37:49 +0200

    verde4_listo

--- app/page2/page.js
+++ app/page2/page.js
@@ -3,8 +3,8 @@ const Page2 = () => {
     return (
       <div>
         
-           <form class="max-w-2xl bg-yellow-600 rounded-lg border p-3 mx-auto mt-20">
-            <p>Page2</p>
+           <form class="max-w-2xl bg-orange-600 rounded-lg border p-3 mx-auto mt-20">
+            <p className="text-xl font-medium">Supabase</p>
       <div class="px-3 mb-2 mt-2">
           <textarea placeholder="comment" class="w-full bg- rounded border border-gray-400 leading-normal resize-none h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"></textarea>
       </div>
