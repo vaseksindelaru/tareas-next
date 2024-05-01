a57ee50a4327e4dd202f0f2f2008db70aa96a68f
vaseksindelaru <vaseksindelaru@gmail.com>
2024-05-01 16:12:16 +0200

    fatz/post,put

--- app/api/tareas/route.js
+++ app/api/tareas/route.js
@@ -3,13 +3,14 @@ import mysql from'mysql2/promise';
 import {NextResponse} from 'next/server'
 
 const conn =  await mysql.createConnection(config);
-export function GET(){
-    return NextResponse.json({message: 'listando tarea'})
+export async function GET(){
+const notasGet = await conn.query ('SELECT * FROM notas')
+    return NextResponse.json(notasGet)
 }
 export async function POST(request){
     const {tema,subtema,descripcion}  = await request.json()
-   const notas1 = await conn.query("INSERT INTO notas SET ?",
+   const notasPost = await conn.query("INSERT INTO notas SET ?",
    {tema:tema,subtema:subtema,descripcion:descripcion});
-   console.log(notas1)
-    return NextResponse.json({message: 'creando tarea'})
+  console.log(notasPost)
+    return NextResponse.json({tema,subtema,descripcion,id: notasPost.insertId})
 }
