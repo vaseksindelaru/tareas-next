c691c16f0dade5a1ecf0aee5bbde73c1cb9bcfee
vaseksindelaru <vaseksindelaru@gmail.com>
2024-05-01 15:03:32 +0200

    fatz/send_notas1_consol.log

--- app/api/tareas/route.js
+++ app/api/tareas/route.js
@@ -1,11 +1,15 @@
-
-
+import {config} from '../../../db/mysql'
+import mysql from'mysql2/promise';
 import {NextResponse} from 'next/server'
+
+const conn =  await mysql.createConnection(config);
 export function GET(){
     return NextResponse.json({message: 'listando tarea'})
 }
 export async function POST(request){
     const {tema,subtema,descripcion}  = await request.json()
-    console.log(data)
+   const notas1 = await conn.query("INSERT INTO notas SET ?",
+   {tema:tema,subtema:subtema,descripcion:descripcion});
+   console.log(notas1)
     return NextResponse.json({message: 'creando tarea'})
 }
